45dde1871baba0de9d080c92693e49fe09c62299
keen <ehrudxo@hotmail.com>
2012-04-26 15:06:38 +0900

    sql update

--- spatialRef_extend.sql
+++ spatialRef_extend.sql
@@ -44,3 +44,5 @@ INSERT INTO spatial_ref_sys(
     PARAMETER["Scale_Factor",1.0],
     PARAMETER["Latitude_Of_Origin",38.0],
     UNIT["Meter",1.0]]', '+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=200000 +y_0=600000 +ellps=GRS80 +units=m +no_defs');
+
+   
